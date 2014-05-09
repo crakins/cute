58035f79972be8ac35b6de9d5cd029f95ba5cc6b
Christopher Akins <rakins@gmail.com>
2014-05-08 23:40:59 -0500

    scrape.py updating to return list of image urls, index displays random scraped image on load

--- app/scrape.py
+++ app/scrape.py
@@ -5,12 +5,37 @@ root_url = 'http://www.bing.com'
 url_prefix_searchTerm = root_url + '/images/search?pq='
 url_mid_searchTerm = '&sc=8-3&sp=-1&sk=&adlt=strict&adlt=strict&adlt=strict&q='
 url_suffix_searchTerm = '&qft=+filterui:imagesize-medium+filterui:license-L2_L3_L4&FORM=R5IR38'
+# index_url = "http://www.bing.com/images/search?pq=kitty&sc=8-3&sp=-1&sk=&adlt=strict&adlt=strict&adlt=strict&q=kitty&qft=+filterui:imagesize-medium+filterui:license-L2_L3_L4&FORM=R5IR38"
 
 #class ImageUrls:
 	#def __init__(self, searchTerm):
 	#	self.searchTerm = searchTerm
-		
+
 def get_urls(searchTerm):
+	index_url = url_prefix_searchTerm + searchTerm + url_mid_searchTerm + searchTerm + url_suffix_searchTerm
+	response = requests.get(index_url)
+	soup = bs4.BeautifulSoup(response.text)
+	
+	links = [a.attrs.get('m') for a in soup.select('a[m]')]
+	
+	###returns a single url for testing
+	##link = links[1].split(',')
+	##url = link[5][8:][:-1]
+	##return url
+	
+	# returns a list of urls
+	url_list = []
+	for l in links:
+		url_list.append(strip_url(l))
+	return url_list
+
+def strip_url(links_item):
+	# find where imgurl: starts
+	startIndex = links_item.find("imgurl:")
+	endIndex = links_item[startIndex:].find('",')
+	return links_item[startIndex + 8:startIndex + endIndex]
+
+def get_urls2(searchTerm):
 	#searchTerm = "kitty"
 	index_url = url_prefix_searchTerm + searchTerm + url_mid_searchTerm + searchTerm + url_suffix_searchTerm
 	response = requests.get(index_url)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,18 @@
 from flask import render_template, flash, redirect
 from app import app
 from scrape import get_urls
+import random
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = { 'nickname': 'Ryan' } # fake user
-	images = get_urls("kitty")
+	searchTerm = "cute puppy"
+	images = get_urls(searchTerm)
+	maxNumber = len(images)
+	randUrl = random.randint(-1, maxNumber)
 	return render_template('index.html', 
 		title = 'Home', 
 		user = user,
-		images = images)
+		searchTerm = searchTerm,
+		images = images[randUrl])
